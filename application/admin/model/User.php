5e7d58e931d3ccc905d7145014a6c5e024066db8
liuyuliang <563021874@qq.com>
2019-04-07 21:02:18 +0800

    second 4.7 on master

--- application/admin/model/User.php
+++ application/admin/model/User.php
@@ -9,7 +9,30 @@
 namespace app\admin\model;
 
 use think\Model;
+use think\model\concern\SoftDelete;
 class User extends Model
 {
+    use SoftDelete;
+    protected $deleteTime = 'delete_time';
+    protected $defaultSoftDelete = 0;
+//构造生成器
+//数据表中角色字段，role返回值处理
+    public function getRoleAttr($value)
+    {
+        $role = [
+            0 => '超级管理员',
+            1 => '管理员',
+        ];
+        return $role[$value];
+    }
 
+    //数据表中状态字段，status返回值处理
+    public function getStatusAttr($value)
+    {
+        $status = [
+            0 => '已停用',
+            1 => '已启用',
+        ];
+        return $status[$value];
+    }
 }
